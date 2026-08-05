b7305167699a27460be1a4e88d7395862ed19858
tecnosh97 <andersonboylouko@gmail.com>
2026-08-05 11:45:52 -0300

    anderson

---
+++ atualizar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "--- INICIANDO ATUALIZAÇÃO DO PROJETO ---"
+
+# Adiciona todas as alterações feitas no código
+git add .
+
+# Pergunta qual foi a alteração para colocar na mensagem do commit
+echo "nada apenas um test:"
+read mensagem
+
+# Faz o commit com a mensagem digitada
+git commit -m "$mensagem"
+
+# Envia para o GitHub na branch main
+git push origin main
+
+echo "--- PROJETO ATUALIZADO COM SUCESSO NO GITHUB! ---"
